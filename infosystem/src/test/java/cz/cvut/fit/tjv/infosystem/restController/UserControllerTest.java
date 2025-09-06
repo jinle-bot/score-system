45dd26f04737d523d2446d50608659497079427d
Jindrich Lechner <lechnjin@fit.cvut.cz>
2025-09-07 00:44:30 +0200

    application.yml divided into dev/test/prod environments files

--- infosystem/src/test/java/cz/cvut/fit/tjv/infosystem/restController/UserControllerTest.java
+++ infosystem/src/test/java/cz/cvut/fit/tjv/infosystem/restController/UserControllerTest.java
@@ -15,6 +15,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
@@ -29,7 +30,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-
+@ActiveProfiles("test")
 @SpringBootTest
 @AutoConfigureMockMvc
 @Import(GlobalExceptionHandler.class)
